0f38e305d990fbad880158d57f30f62ffde0c01e
Alain Nkazamurego <alain.nkazamurego@gmail.com>
2023-06-07 02:55:17 +0300

    Let fix alphabet and check again

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for a in range(97, 97 + 26):
-    if a == 113 or a == 101:
-        continue
-    print("{:c}".format(a), end='')
+for i in range(ord('a'), ord('z') + 1):
+    if ord('q') != i and ord('e') != i:
+        print("{}".format(chr(i)), end='')
